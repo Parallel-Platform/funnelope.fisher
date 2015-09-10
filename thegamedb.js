6e4a9c4df612355bb3942d2b70df3d4d4eaad3e9
Michael Onubogu <nonubogu@gmail.com>
2015-09-10 18:23:35 -0400

    funnelope init - latinizer hommie
    I just had to save this shizneet yo!!

---
+++ thegamedb.js
@@ -0,0 +1,29 @@
+﻿var rp = require('request-promise');
+var config = require('./config.js');
+var http = require('http');
+var url = require('url');
+
+var gamesdb = {
+    
+    getGame: function (id){
+    },
+
+    searchGames: function (query){
+        if (query !== null && query !== undefined && query !== '') {
+            
+            var gameDbRef = config.thegamedb;
+            var gameDbUrl = gameDbRef.url + gameDbRef.endpoints.GetGamesList + '?name=' + encodeURIComponent(query.toLowerCase());
+            
+            options = {
+                uri : gameDbUrl,
+                timeout : 1800000
+            }
+            
+            console.log('calling GamesDB.net');
+            return rp(options);
+        }
+        return null;
+    }
+};
+
+module.exports = gamesdb;
